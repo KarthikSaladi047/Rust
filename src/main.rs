5b61aac243156e83c02333f77d90e083ae49ff5f
Karthik Saladi <105864615+KarthikSaladi047@users.noreply.github.com>
2024-01-15 16:35:02 +0000

    done sec 2

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,25 @@
 mod data_types;
 mod operators;
 mod scope_and_shadowing;
+mod stack_heap;
 
 
-const AGE:u8 = 23; //no fixed address, globally available, must specify type and variable has tio be in upeercase snake case notation
-static mut LAST_YEAR:u32 = 2023;
-static NEW_YEAR:u32 = 2024;
-fn main() {
+fn main(){
     // data_types::variable_types();
     // operators::operators();
     // scope_and_shadowing::scope_and_shadowing();
-    println!("age is {}", AGE);
-    unsafe  {
-        println!("good bye {}", LAST_YEAR);
-    }
-    println!("welcome {}", NEW_YEAR);
+    stack_heap::stack_heap();
 }
+
+
+// const AGE:u8 = 23; //no fixed address, globally available, must specify type and variable has to be in upeercase snake case notation
+// static mut LAST_YEAR:u32 = 2023;
+// static NEW_YEAR:u32 = 2024;
+// fn main() {
+
+//     println!("age is {}", AGE);
+//     unsafe  {
+//         println!("good bye {}", LAST_YEAR);
+//     }
+//     println!("welcome {}", NEW_YEAR);
+// }
